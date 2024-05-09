e88e7fc3b2f3c28383d9dbfb1be008fe2266167e
Prachi Heda <prachiheda@gmail.com>
2024-05-09 16:36:47 -0700

    signup auth
    Update package-lock.json
    
    Changes made after installing packages
    
    Fixed Some Grammatical Errors
    
    fix missing email
    
    update login
    
    remove secure info
    
    added password confirmation + changed absolute path to relative
    
    Revised Login + Signup Page
    
    Handles bad signup
    Handles bad login
    
    Edit SignupPage + Small Revisions LoginPage
    
    SignupPage:
    - Rewrite if/else to Switch case
    - Specific identifier for incorrect prompts
    -Successful Sign-up will route to Log-In page
    
    Login page:
    - Specific identifier for incorrect prompts
    - Successful login will now refresh the page and have an empty prompt
    
    Update LoginPage.tsx
    
    LoginPage will now refresh upon a successful login

---
+++ src/pages/Signup/SignupForm.tsx
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+
+import { FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+import {auth} from "../../firebase"
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
+
+export const SignupForm = () => {
+  const [email, setEmail] = useState(''); 
+  const [password, setPassword] = useState(''); 
+  const handleSubmit = async (e: { preventDefault: () => void; }) =>{
+    e.preventDefault()
+    try{
+      createUserWithEmailAndPassword(auth, email,password)
+      console.log("account created")
+    } catch(err){
+      console.log(err)
+    }
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormControl mb={4}>
+        <FormLabel>Username</FormLabel>
+        <Input type="email" onChange = {(e)=>setEmail(e.target.value)}/>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Password</FormLabel>
+        <Input type="password" onChange = {(e)=>setPassword(e.target.value)}/>
+      </FormControl>
+      <Button mt={6} colorScheme="blue" type="submit" w="100%">
+        Sign Up
+      </Button>
+    </form>
+  );
+};
--- src/pages/Signup/SignupPage.tsx
+++ src/pages/Signup/SignupPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import {
   Container,
   FormControl,
@@ -7,13 +8,16 @@ import {
   Button,
   Text,
   Box,
-  Center,
   Stack,
   SimpleGrid,
+  Wrap,
+  WrapItem,
 } from '@chakra-ui/react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { Link } from '@src/components/common';
 import { AuthLayout } from '@src/components/layouts';
 import { ROUTE_PATHS } from '@src/constants/routes.constants';
+import { auth } from '@src/firebase';
 
 enum Role {
   MENTOR = 'Mentor',
@@ -24,6 +28,69 @@ const RoleValues = Object.values(Role);
 
 export const SignupPage = () => {
   const [selectedRole, setSelectedRole] = useState<Role>(Role.MENTOR);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setPasswordError('Passwords do not match.');
+
+      setTimeout(() => {
+        setPasswordError('');
+      }, 5000);
+      return; // Stop the form submission
+    }
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      console.log('Account Created Successfully');
+      navigate(ROUTE_PATHS.LOGIN);
+    } catch (err: any) {
+      switch (err.code) {
+  
+        case 'auth/missing-email':
+          console.error('Missing email');
+          setPasswordError('Missing email');
+          setTimeout(() => {
+            setPasswordError('');
+          }, 5000);
+          break;
+  
+        case 'auth/missing-password':
+          setPasswordError('Missing password');
+          setTimeout(() => {
+            setPasswordError('');
+          }, 5000);
+          break;
+  
+        case 'auth/email-already-in-use':
+          console.error('Email already in use');
+          setPasswordError('This email is already in use. Please use a different email');
+          setTimeout(() => {
+            setPasswordError('');
+          }, 5000);
+          break;
+  
+        case 'auth/weak-password':
+          console.error('Weak password');
+          setPasswordError('Password must be at least 6 characters. Please try a different password');
+          setTimeout(() => {
+            setPasswordError('');
+          }, 5000);
+          break;
+
+        default:
+          console.error('Signup failed: ', err.message);
+          setPasswordError('Failed to create account. Please try again later');
+          setTimeout(() => {
+            setPasswordError('');
+          }, 5000);
+      }
+    }
+  };
 
   const renderRoleButton = () =>
     RoleValues.map((role) => (
@@ -53,18 +120,19 @@ export const SignupPage = () => {
         </Stack>
 
         <Box mt={10} mb={6}>
-          <form>
+          <form onSubmit={handleSubmit}>
             <FormControl mb={4}>
               <FormLabel>Username</FormLabel>
-              <Input type="email" />
+              <Input type="email" onChange={(e) => setEmail(e.target.value)} />
             </FormControl>
-            <FormControl>
+            <FormControl mb={4}>
               <FormLabel>Password</FormLabel>
-              <Input type="password" />
+              <Input type="password" onChange={(e) => setPassword(e.target.value)} />
             </FormControl>
-            <FormControl>
+            <FormControl mb={4}>
               <FormLabel>Confirm Password</FormLabel>
-              <Input type="password" />
+              <Input type="password" onChange={(e) => setConfirmPassword(e.target.value)} />
+              {passwordError && <Text color="red.500">{passwordError}</Text>}
             </FormControl>
             <Button mt={6} colorScheme="blue" type="submit" w="100%">
               Sign Up
@@ -72,11 +140,14 @@ export const SignupPage = () => {
           </form>
         </Box>
 
-        <Center textAlign="center">
-          <Text as="b">
-            <Link to={ROUTE_PATHS.LOGIN}>Already have an account?</Link>
-          </Text>
-        </Center>
+        <Wrap as="b" justify="center">
+          <WrapItem>
+            <div>Already have a SaigonChildren account?</div>
+          </WrapItem>
+          <WrapItem>
+            <Link to={ROUTE_PATHS.LOGIN}>Log in</Link>
+          </WrapItem>
+        </Wrap>
       </Container>
     </AuthLayout>
   );
